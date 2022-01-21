dbb9867da55a928723967d6118e54b6be0f63a26
tedyted26 <tedyted26@hotmail.com>
2022-01-21 23:12:42 +0100

    fix

--- vista/clasificador.py
+++ vista/clasificador.py
@@ -153,6 +153,7 @@ class Clasificador_frame(ttk.Frame):
         self.label_error.config(text="")
         ruta_noticias = self.texto_noticias.get(1.0, "end-1c")
         ruta_modelo = self.texto_modelo.get(1.0, "end-1c")
+        self.lista_noticias.delete(*self.lista_noticias.get_children())
 
         if ruta_noticias == "" or ruta_modelo == "" or not Path(ruta_noticias).exists() or not Path(ruta_modelo).exists():
             self.label_error.config(text = "Comprueba los campos. No se ha proporcionado una ruta correcta.")
