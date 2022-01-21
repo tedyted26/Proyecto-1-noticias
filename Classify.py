c3c7fb951686c3814ce1e99ff5c2e93933924071
tedyted26 <tedyted26@hotmail.com>
2022-01-21 17:30:23 +0100

    aaa

--- Classify.py
+++ Classify.py
@@ -1,4 +1,5 @@
 import pickle
+import tratamientoNoticias as tn
 
 
 class Classify():
@@ -8,6 +9,16 @@ class Classify():
     def classifyNews(self, pathNoticias, model):
         # estaria bien que fuera un diccionario de clave valor, siendo el valor si es de odio o no, y la clave la ruta de la noticia
         # importante guardar el tiempo que tarda el algoritmo en ejecutarse
+        vectores = []
+        paths = tn.getAllNewsUrlList(pathNoticias)[:10]
+        for n, i in enumerate(paths):
+            try:
+                textoNoticia = tn.leerNoticia(i[0])
+                vectores.append(tn.generarVectorDeTexto(textoNoticia, True, i[1], odio= -1))
+            except:
+                print(f"Error generando vector en archivo: {i[1]}")
+
+        lista_valores = model.predict(X)
         resultados = {}
         tiempo = 0
 
